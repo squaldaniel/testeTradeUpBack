7f414a5d25ba79d374f095b8a1b68369d597ce60
Daniels Hogans <daniel.santos.ap@gmail.com>
2024-02-26 09:58:09 -0300

    finalizado

--- routes/api.php
+++ routes/api.php
@@ -4,16 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ViaCepController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 Route::middleware('localcors')->group(function () {
     Route::get('getcep/{cep}', [ViaCepController::class, 'getCep']);
 });
